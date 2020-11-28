69849d5133339bcd08e62af026e09a48cab676a9
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-11-28 12:53:11 +0300

    add 3-deployment example
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- examples/3-compose/python-redis/app.py
+++ examples/3-compose/python-redis/app.py
@@ -21,4 +21,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Changed! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen {} times.\n'.format(count)
